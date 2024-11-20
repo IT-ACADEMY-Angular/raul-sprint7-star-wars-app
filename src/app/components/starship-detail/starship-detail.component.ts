0fe7a80952049c0141775848813579fbd4cbdd51
rawl0408 <raulin4bcn@gmail.com>
2024-11-20 13:57:55 +0100

    Sprint 7 maquetación V1

---
+++ src/app/components/starship-detail/starship-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'starship-detail-component',
+  imports: [],
+  templateUrl: './starship-detail.component.html',
+  styleUrl: './starship-detail.component.css'
+})
+export class StarshipDetailComponent {
+
+}
---
+++ src/app/components/starship-list/starship-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'starship-list-component',
+  imports: [],
+  templateUrl: './starship-list.component.html',
+  styleUrl: './starship-list.component.css'
+})
+export class StarshipListComponent {
+
+}
